c28270ee8c022190def125aeb02f7dd684a7d60f
Gabriel Almeida <gabrielsantos.almeida@hotmail.com>
2021-02-04 22:09:51 -0300

    feat: calendar and stylizations

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,13 +1,20 @@
-import React, { useState } from 'react';
+import React, { useCallback, useState } from 'react';
 import { FiClock, FiPower } from 'react-icons/fi';
+import DayPicker, { DayModifiers } from 'react-day-picker'
+import 'react-day-picker/lib/style.css';
 
 import { Container, Header, HeaderContent, Profile, Content, Schedule, Calendar, NextAppointment, Section, Appointment } from './styles';
 import logoImg from '../../assets/logo.svg';
 import { useAuth } from '../../hooks/auth';
 
-
 const Dashboard: React.FC = () => {
   const [selectDate, setSelectDate] = useState(new Date());
+
+  const handleDateChange = useCallback((day: Date, modifiers: DayModifiers) => {
+    if (modifiers.available) {
+      setSelectDate(day);
+    }
+  }, []);
 
   const { sigOut, user } = useAuth();
 
@@ -116,7 +123,32 @@ const Dashboard: React.FC = () => {
               </Appointment>
             </Section>
           </Schedule>
-          <Calendar />
+          <Calendar>
+            <DayPicker
+              weekdaysShort={['D', 'S', 'T', 'Q', 'Q', 'S', 'S']}
+              fromMonth={new Date()}
+              disabledDays={[{ daysOfWeek: [0, 6] }]}
+              modifiers={{
+                available: { daysOfWeek: [1, 2, 3, 4, 5] }
+              }}
+              onDayClick={handleDateChange}
+              selectedDays={selectDate}
+              months={[
+                'Janeiro',
+                'Fevereiro',
+                'Março',
+                'Abril',
+                'Maio',
+                'Junho',
+                'Julho',
+                'Agosto',
+                'Setembro',
+                'Outubro',
+                'Novembro',
+                'Dezembro',
+              ]}
+            />
+          </Calendar>
         </Content>
       </Header>
     </Container>
